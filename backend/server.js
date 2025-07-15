8c88b6936452a52b8445f2afdab4129b750b25e2
YOUR NAME HERE <ksj4@sfu.ca>
2025-07-15 16:06:31 -0700

    Created server.js file for backend with starter template

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+// starter template
+require('dotenv').config(); // Loads .env variables
+
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const PORT = 4000;
+
+app.use(cors());
+app.use(express.json());
+
+// Example route to test
+app.get('/', (req, res) => {
+  res.send('HealthMate Backend is running!');
+});
+
+// to add Edamam routes later here..
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on http://localhost:${PORT}`);
+});
