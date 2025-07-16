b856c89ff4fadf4f05f395748715a15f7350197e
jbn1440 <justinborgeson0@icloud.com>
2025-07-16 04:01:03 -0700

    Added basic search functionality

--- react/healthmate/src/pages/AnalyzeMeal.jsx
+++ react/healthmate/src/pages/AnalyzeMeal.jsx
@@ -1,4 +1,14 @@
 import NavBar from '../components/NavBar'
+import {useState} from 'react'
+
+function SearchResult({food}) {
+    return (
+        <div>
+            <p>{food.name}</p>
+            <button type="button">+</button>
+        </div>
+    )
+}
 
 function NutritionalInfo({food}) {
     return (
@@ -15,8 +25,11 @@ function NutritionalInfo({food}) {
 }
 
 function AnalyzeMeal() {
+    const [searchQuery, setSearchQuery] = useState("");
+    
     const meals = [
         {
+            id: 0,
             name: "apple",
             serving: 182,
             calories: 95,
@@ -24,6 +37,7 @@ function AnalyzeMeal() {
             carbohydrates: 25
         },
         {
+            id: 1,
             name: "bread",
             serving: 182,
             calories: 95,
@@ -32,11 +46,33 @@ function AnalyzeMeal() {
         }
     ]
     
+    const handleSearch = (e) => {
+        e.preventDefault()
+    };
+
     return (
         <>
             <NavBar/>
             <div className="analyzeMeal">
-                <div className="searchBar"></div>
+                <div className="searchMenu">
+                    <form onSubmit={handleSearch} className="searchForm">
+                        <input
+                            type="text"
+                            placeholder="Search for meals..."
+                            className="searchInput"
+                            value={searchQuery}
+                            onChange={(e) => setSearchQuery(e.target.value)}
+                        />
+                    </form>
+                    <div className="searchResults">
+                        {meals.map(
+                            (food) =>
+                                food.name.toLowerCase().startsWith(searchQuery) && (
+                                    <SearchResult food={food} key={food.id} />
+                                )
+                        )}
+                    </div>
+                </div>
                 <div className="nutritionalInfo">
                     <NutritionalInfo food={meals[0]}/>
                 </div>
