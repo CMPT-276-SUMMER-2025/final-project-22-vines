e3bb0c5a509c35a97472c29d6526fd985164da24
jbn1440 <justinborgeson0@icloud.com>
2025-07-16 02:42:15 -0700

    Setup meal analyzer page

---
+++ react/healthmate/src/pages/AnalyzeMeal.jsx
@@ -0,0 +1,11 @@
+function AnalyzeMeal() {
+    return (
+        <>
+            <NavBar/>
+            <div className="analyzeMeal">
+            </div>
+        </> 
+    )
+}
+
+export default AnalyzeMeal
