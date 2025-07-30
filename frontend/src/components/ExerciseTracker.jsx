27be4f2464b540b3db21e6edf899f10bbbafe0bf
jbn1440 <justinborgeson0@icloud.com>
2025-07-30 07:53:47 -0700

    Setup work in progress track exercise page

---
+++ frontend/src/components/ExerciseTracker.jsx
@@ -0,0 +1,10 @@
+import "../css/TrackExercise.css";
+
+export default function ExerciseTracker() {
+
+  return (
+    <div className="trackExercise">
+        <p>Work in progress.</p>
+    </div>
+  );
+}
--- frontend/src/pages/TrackExercise.jsx
+++ frontend/src/pages/TrackExercise.jsx
@@ -1,9 +1,12 @@
+import ExerciseTracker from '../components/ExerciseTracker';
 import NavBar from '../components/NavBar'
+import "../css/TrackExercise.css";
 
 function TrackExercise() {
     return (
         <>
             <NavBar/>
+            <ExerciseTracker/>
         </> 
     )
 }
