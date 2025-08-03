0825e5b87270676fdb2c4bd64475ba5f8d1f44d9
jbn1440 <justinborgeson0@icloud.com>
2025-08-02 20:32:14 -0700

    Setup new navbar

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,5 +1,5 @@
 import {Link} from "react-router-dom";
-import {NavLink} from "react-router-dom";
+import {useLocation, NavLink} from "react-router-dom";
 import logo from "../assets/logo.png";
 import name from "../assets/name.png";
 import dashboard from "../assets/dashboard.svg";
@@ -20,23 +20,49 @@ function NavBarButton({destination, buttonIcon, buttonText}) {
     )
 }
 
-function NavBar() {
-    return (
-        <nav className="navbar">
-            <Link to="/" className="fullLogo">
-                <img src={logo} alt="Logo" className="logo"/>
-                <img src={name} alt="Name" className="name"/>
-            </Link>
-            <div className="navbarLinks">
-                <NavBarButton destination={""} buttonIcon={dashboard} buttonText={"Dashboard"}/>
-                <NavBarButton destination={"logfood"} buttonIcon={logfood} buttonText={"Log Food"}/>
-                <NavBarButton destination={"analyzemeal"} buttonIcon={analyzemeal} buttonText={"Analyze Meal"}/>
-                <NavBarButton destination={"trackexercise"} buttonIcon={trackexercise} buttonText={"Track Exercise"}/>
-                
-            </div>
-            <NavBarButton destination={"settings"} buttonIcon={settings} buttonText={"Settings"}/>
-        </nav>
-    )
+function Dropdown({ icon, label, children, routes = [] }) {
+  const location = useLocation();
+  const isActive = routes.some((route) => location.pathname.startsWith(`/${route}`));
+
+  return (
+    <div className={`dropdown ${isActive ? "activeDropdown" : ""}`}>
+      <button type="button" className="dropdownButton">
+        <img src={icon} alt={`${label} icon`} className="buttonIcon" />
+        {label}
+      </button>
+      <div className="dropdownContent">{children}</div>
+    </div>
+  );
 }
+
+
+function NavBar() {
+  return (
+    <nav className="navbar">
+      <Link to="/" className="fullLogo">
+        <img src={logo} alt="Logo" className="logo" />
+        <img src={name} alt="Name" className="name" />
+      </Link>
+
+      <div className="navbarLinks">
+        <NavBarButton destination={""} buttonIcon={dashboard} buttonText={"Dashboard"} />
+
+        <Dropdown icon={logfood} label="Nutrition" routes={["logfood", "analyzemeal"]}>
+        <NavBarButton destination={"logfood"} buttonIcon={logfood} buttonText={"Log Food"} />
+        <NavBarButton destination={"analyzemeal"} buttonIcon={analyzemeal} buttonText={"Analyze Meal"} />
+        </Dropdown>
+
+        <Dropdown icon={trackexercise} label="Exercise" routes={["logexercise", "searchexercises", "generateplan"]}>
+        <NavBarButton destination={"logexercise"} buttonIcon={trackexercise} buttonText={"Log Exercise"} />
+        <NavBarButton destination={"searchexercises"} buttonIcon={trackexercise} buttonText={"Search Exercises"} />
+        <NavBarButton destination={"generateplan"} buttonIcon={trackexercise} buttonText={"Generate Routine"} />
+        </Dropdown>
+      </div>
+
+      <NavBarButton destination={"settings"} buttonIcon={settings} buttonText={"Settings"} />
+    </nav>
+  );
+}
+
 
 export default NavBar
