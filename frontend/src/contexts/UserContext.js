3eabb2615cff371b7ee5eb500011d31722bde50d
jbn1440 <justinborgeson0@icloud.com>
2025-08-06 12:32:49 -0700

    Fixed exercise history

--- frontend/src/contexts/UserContext.js
+++ frontend/src/contexts/UserContext.js
@@ -1,11 +1,17 @@
-import { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useState } from 'react';
 
 export const UserContext = createContext();
 
 export const useUser = () => useContext(UserContext);
 
 export function UserProvider({ children }) {
-  const [phone, setPhone] = useState(null);
+  const [phone, setPhoneState] = useState(localStorage.getItem('phone') || null);
+
+  const setPhone = (value) => {
+    setPhoneState(value);
+    localStorage.setItem('phone', value);
+  };
+
   return (
     <UserContext.Provider value={{ phone, setPhone }}>
       {children}
